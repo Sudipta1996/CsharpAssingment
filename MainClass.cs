4e3821b75e650be295ce2c6da10292b339eb406d
Sudipta Sen <48800496+Sudipta1996@users.noreply.github.com>
2022-04-17 20:33:59 +0530

    Csharp OOps

---
+++ MainClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpOOPClassAssignment
+{
+     class MainClass
+    {
+        public delegate void EmployeeDelegate(Employee employee);
+        public static void PrintDetails(Employee employee)
+        {
+            if (employee is Manager)
+            {
+                Console.WriteLine("Manager Details are:" + employee.EmpName + " " + employee.EmpNo + " " + employee.GrossSalary);
+            }
+        }
+        public static void PrintDetailsMarketingExecutive(Employee employee)
+        {
+            if (employee is MarketingExecutive)
+            {
+                Console.WriteLine("Marketing Details are:" + employee.EmpName + " " + employee.EmpNo+  " "  +employee.GrossSalary);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine(" Enter the id :" );
+            employee.EmpNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Name : " );
+            employee.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter  salary For Employee : " );
+            employee.Salary = Convert.ToDouble(Console.ReadLine());
+            employee.calculateSalary();
+            Console.WriteLine("Gross Salary of Employee :" + employee.GrossSalary);
+            Console.WriteLine("Net Salary of Employee :" + employee.NetSalary);
+            
+
+            Manager manager = new Manager();
+            Console.WriteLine("Enter the id of manager: ");
+            manager.EmpNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the name of manager: ");
+            manager.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter the salary of manager: ");
+            manager.Salary = Convert.ToDouble(Console.ReadLine());
+            manager.calculateSalary();
+            Console.WriteLine("Gross Salary of Manager :" + manager.GrossSalary);
+            //EmployeeDelegate employeeDelegate = PrintDetails;
+            //employeeDelegate(manager);
+
+
+            MarketingExecutive marketingExecutive = new MarketingExecutive();
+            Console.WriteLine("Enter the id of marketingExecutive: ");
+            marketingExecutive.EmpNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the name of marketingExecutive: ");
+            marketingExecutive.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter the salary of marketingExecutive: ");
+            marketingExecutive.Salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the kilometers of marketingExecutive: ");
+            marketingExecutive.kilo = Convert.ToDouble(Console.ReadLine());
+            marketingExecutive.calculateSalary();
+            Console.WriteLine("Gross Salary of MarketingExecutive :" + marketingExecutive.GrossSalary);
+
+            EmployeeDelegate employeeDelegate1 = PrintDetails;
+            EmployeeDelegate employeeDelegate2 = PrintDetailsMarketingExecutive;
+            EmployeeDelegate employeeDelegate = employeeDelegate1 + employeeDelegate2;
+            employeeDelegate(manager);
+            employeeDelegate(marketingExecutive);
+            Console.ReadKey();
+
+        }
+    }
+    
+}
---
+++ MyStack.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpOOPClassAssignment
+{
+	internal class MyStack
+	{
+
+
+		readonly int MAX = 1000;
+		int top;
+		int[] stack = new int[1000];
+
+		public MyStack()
+		{
+			top = -1;
+		}
+		public bool Push(int data)
+		{
+			if (top + 1 >= MAX)
+			{
+				Console.WriteLine("Stack is Full");
+				return false;
+			}
+			else
+			{
+				stack[++top] = data;
+				return true;
+			}
+		}
+
+		public int Pop()
+		{
+			if (top < 0)
+			{
+				Console.WriteLine("Stack is Empty");
+				return 0;
+			}
+			else
+			{
+				int value = stack[top--];
+				return value;
+			}
+		}
+
+
+
+		public void PrintStack()
+		{
+			if (top < 0)
+			{
+				Console.WriteLine("Stack is empty");
+				return;
+			}
+			else
+			{
+				Console.WriteLine(" Stack  Items are :");
+				for (int i = top; i >= 0; i--)
+				{
+					Console.WriteLine(stack[i]);
+				}
+			}
+		}
+
+
+
+		static void Main(string[] args)
+		{
+			MyStack myStack = new MyStack();
+
+			myStack.Push(10);
+			myStack.Push(20);
+			myStack.Push(30);
+			myStack.Push(40);
+			myStack.Push(50);
+			myStack.Push(100);
+			myStack.PrintStack();
+			Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
+			myStack.PrintStack();
+			Console.ReadKey();
+		}
+	}
+}
+
