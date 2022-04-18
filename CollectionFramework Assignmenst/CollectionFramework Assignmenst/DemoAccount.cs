0238dffd032f405d8b0b0e538156ba65b3f79dc6
Your Name <you@example.com>
2022-04-18 14:55:11 +0530

    all commited to main

--- CollectionFramework Assignmenst/CollectionFramework Assignmenst/DemoAccount.cs
+++ CollectionFramework Assignmenst/CollectionFramework Assignmenst/DemoAccount.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,17 +8,21 @@ using System.Threading.Tasks;
 namespace CollectionFramework_Assignmenst
 {
     public class DemoAccount
+        
     {
+
+        String myPath = @"D:\Test_2.txt";
         public string AccountNumber { get; set; }
         public string CustomerName { get; set; }
         public int Balance { get; set; } = 0;
-
+        
         public event EventHandler Underbalance;
         public event EventHandler BalanceZero;
 
 
         public void Withdraw(int amt)
         {
+            StreamWriter sw = new StreamWriter(myPath, true);
             Console.WriteLine("Enter the Ammount to Withdrawn :");
             amt = Convert.ToInt32(Console.ReadLine());
 
@@ -29,12 +34,15 @@ namespace CollectionFramework_Assignmenst
                 }
                 Balance -= amt;
                 Console.WriteLine("Successfully Withdrawn from Account." + amt);
+                sw.WriteLine("Successfully Withdrawn from Account." + amt);
                 Console.WriteLine("\nCurrent Balance:" + CheckBalance());
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            sw.Close();
+            
         }
 
         private int CheckBalance()
@@ -53,14 +61,15 @@ namespace CollectionFramework_Assignmenst
 
         public void Deposite(int amt)
         {
-
+            StreamWriter sw = new StreamWriter(myPath, true);
             Console.WriteLine("Enter the Ammount to Deposit :");
             amt = Convert.ToInt32(Console.ReadLine());
             Balance += amt;
             Console.WriteLine("Successfully deposited into account." + amt);
+            sw.WriteLine("Successfully Withdrawn from Account." + amt);
             Console.WriteLine("Current Amount:" + Balance);
-
-
+            sw.Close();
+            
         }
         public static void UnderBalanceAlert(object sender, EventArgs e)
         {
@@ -72,9 +81,19 @@ namespace CollectionFramework_Assignmenst
             Console.WriteLine("\nZero Balance Alert!! Account balance: 0");
         }
 
+        //public void ReadFromFile()
+        //{
+        //    StreamReader sr = new StreamReader(myPath);
+        //    while (!sr.EndOfStream)
+        //    {
+        //        Console.WriteLine(sr.ReadLine());
+        //    }
+
+        //}
 
         static void Main(string[] args)
         {
+            
 
             DemoAccount account = new DemoAccount()
             {
@@ -83,9 +102,12 @@ namespace CollectionFramework_Assignmenst
                 CustomerName = "Pradip",
                 Balance = 10000
             };
-                Console.WriteLine("Customer Acc No :" + " " + account.AccountNumber + "\tCustomer Name :" + " " + account.CustomerName + "\tCustomer Balance :" + " " + account.Balance);
+               Console.WriteLine("Customer Acc No :" + " " + account.AccountNumber + "\tCustomer Name :" + " " + account.CustomerName + "\tCustomer Balance :" + " " + account.Balance);
                 account.Underbalance += UnderBalanceAlert;
                 account.BalanceZero += BalanceZeroAlert;
+
+            //account.ReadFromFile();
+
 
             string choice;
             Console.WriteLine("Please enter a Choice: For Deposit Press d : For Withdraw Press w (d,w):");
@@ -101,7 +123,7 @@ namespace CollectionFramework_Assignmenst
                     break;
                 
             }
-                
+             
              Console.ReadKey();
             
 
